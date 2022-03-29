64e10db2c4253aceb92380ff10ebd8eadbb4e868
kabilan.sv <kabilan.sv@zohocorp.com>
2022-03-29 22:26:24 +0530

    EPI Mar 29, stock once

---
+++ buy_and_sell_stock.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from test_framework import generic_test
+
+
+def buy_and_sell_stock_once(prices: List[float]) -> float:
+    minPrice,maxProfit = float('inf'), 0.0
+    for price in prices:
+        maxProfitToday = price - minPrice
+        maxProfit = max(maxProfit, maxProfitToday)
+        minPrice = min(minPrice,price)
+    return maxProfit
+
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('buy_and_sell_stock.py',
+                                       'buy_and_sell_stock.tsv',
+                                       buy_and_sell_stock_once))
